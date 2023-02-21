117b5d703ced8940ad846ad534e31e5d2b8b5734
Nayely10 <113447338+Nayely10@users.noreply.github.com>
2023-02-21 12:38:51 -0500

    Add files via upload
    https://www.freecodecamp.org/espanol/news/el-manual-de-arreglos-en-javascript/

---
+++ VECTORES/vectores.js
@@ -0,0 +1,32 @@
+//intro vectores / arreglos
+/*var fruta1 = "mango";
+var fruta2 = "pera";
+var fruta3 = "banano";
+// frutas es una variable 
+var frutas = "";
+for( var i= 0; i < 3 ;i++){
+    var f=prompt("digite una fruta");
+    frutas = frutas + f + ",";
+}
+document.write("Las frutas:"+frutas);
+
+//vector frutas
+*/
+/*frutas_vectores=[ "Sandia","Fresas","Piñas"];
+datos = [
+    344434,
+    false,
+    "texto",
+    function saludo (){
+        alert("hola");0
+    },
+    ["texto 1",76,true]
+
+]
+document.write("DATOS GUARDADOS:"+ datos [4][0]);
+*/
+///recorrer vectores
+var n = [5,7,10];
+for (var i=0; i = n.length; i++){
+document.write("suma vectores:"+ (n[i]*2)+"<br>")
+}
